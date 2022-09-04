fa3c8ff3cc1074252240b8d0fa6eeb0105e556b3
asishupadhyay24@gmail.com <India@wc2019>
2022-09-04 13:27:13 +0530

    master -r=self first push after development

---
+++ server.js
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from 'apollo-server';
+import typeDefs  from './typeDefs.js';
+import resolvers from './resolvers.js';
+//import mysql from mysql
+import jwt from 'jsonwebtoken';
+
+
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context:({req})=>{
+        const {authorization} =  req.headers
+        if(authorization){
+         const {userId} =  jwt.verify(authorization,process.env.JWT_SECRET)
+         return {userId}
+        }
+     }
+  });
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
